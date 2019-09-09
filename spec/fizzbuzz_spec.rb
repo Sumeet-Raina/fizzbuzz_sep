40b21ec3faea0b8d0ce5d9d146918e8a08460c5b
sumeet978 <searchsumeet@gmail.com>
2019-09-09 20:55:26 +0100

    fixed indentetion

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,10 +5,10 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   it 'returns "buzz" when passes 5'do 
-  expect(fizzbuzz(5)).to eq 'buzz'
-end
-it 'returns "fizzbuzz" when passes 3 && 5'do 
-  expect(fizzbuzz(15)).to eq 'fizzbuzz'
-end
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passes 3 && 5'do 
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 
 end
