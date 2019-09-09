616e0d73450d4d65a132b4cc2f67619c7960629c
sumeet978 <searchsumeet@gmail.com>
2019-09-09 20:54:09 +0100

    All tests pass, refactored the code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,11 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passes 5'do 
+  expect(fizzbuzz(5)).to eq 'buzz'
+end
+it 'returns "fizzbuzz" when passes 3 && 5'do 
+  expect(fizzbuzz(15)).to eq 'fizzbuzz'
+end
+
 end
